6fa4bc5a1391ee75d4f535e8fa6f27f66e808dd7
Nalla Suman <aliensgroup04@gmail.com>
2025-03-03 15:51:39 +0530

    Update cooking.py

--- cooking.py
+++ cooking.py
@@ -12,7 +12,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field, ValidationError
 from typing import List
-GOOGLE_API_KEY = "AIzaSyCBhbuJbxjlghoZ3X1HQhS_qwuMpSE1wC0"
 
 def stream_response(response_generator):
     response_text = ""
@@ -31,7 +30,7 @@ class Recipe(BaseModel):
 output_parser = PydanticOutputParser(pydantic_object=Recipe)
 
 # Load API key securely
-model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=st.secrets["GOOGLE_API_KEY"])
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=st.secrets["AIzaSyCBhbuJbxjlghoZ3X1HQhS_qwuMpSE1wC0"])
 
 # Prompt Template
 prompt_template = ChatPromptTemplate(
